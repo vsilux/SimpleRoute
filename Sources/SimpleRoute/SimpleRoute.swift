baf00ca5c1c14d566819f8893676a3c7ca32cc98
Ilya Suvorov <vsilux@gmail.com>
2025-06-09 17:48:17 +0300

    marked route provider as public

--- Sources/SimpleRoute/SimpleRoute.swift
+++ Sources/SimpleRoute/SimpleRoute.swift
@@ -37,7 +37,7 @@ public protocol Route: Hashable, Identifiable {
     var destination: Destination { get }
 }
 
-typealias RouteProvider = (any Route) -> Void
+public typealias RouteProvider = (any Route) -> Void
 
 public struct AnyRoute: Route, Hashable {
     private let wrappedRoute: any Route
